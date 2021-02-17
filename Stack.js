bd59e6264d9a834843a966e66d6cc655353003e2
Joel Del Cueto <wayfaringjou@gmail.com>
2021-02-17 15:51:34 -0500

    Add Stack and Queue classes with drills

---
+++ Stack.js
@@ -0,0 +1,24 @@
+class _Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(data) {
+    if (this.top === null) {
+      this.top = new _Node(data, null);
+      return this.top;
+    }
+
+    const node = new _Node(data, this.top);
+    this.top = node;
+  }
+}
+
+module.exports = Stack;
